185e2e6ad5ec4e7ffeacfb5b9b01aa45daf823c6
andredefariadeandrade <andre.faria.andrade@gmail.com>
2022-09-13 00:40:00 +0100

    fim sa seção 5

--- aula60/index.js
+++ aula60/index.js
@@ -1,5 +1,8 @@
-const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const a1 = [10, 20, 30];
+let total = 0;
 
-a1.forEach((valor, indice, array) => {
-    console.log(valor, indice, array);
+a1.forEach((valor) => {
+    total += valor;
 });
+
+console.log(total);
