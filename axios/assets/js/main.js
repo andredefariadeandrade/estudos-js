6bb137b612318a1c3f1cf7873f0e4389e6485818
andredefariadeandrade <andre.faria.andrade@gmail.com>
2022-09-21 14:57:26 +0100

    json

--- axios/assets/js/main.js
+++ axios/assets/js/main.js
@@ -1,29 +1,42 @@
-document.addEventListener("click", (e) => {
-    const el = e.target;
-    const tag = el.tagName.toLowerCase();
+// buscamos os dados de pessoas.json
+fetch("pessoas.json")
+  // armazenamos o resultado na variavel resposta e convertemos essa resposta para formato json
+  .then((resposta) => resposta.json())
+  // então jogamos o resultado convertido para a variavel json e jogamos pra dentro de uma function
+  .then((json) => carregaElementosNaPagina(json));
 
-    if (tag === "a") {
-        e.preventDefault();
-        carregaPagina(el);
-    }
-});
+  // criamos a função que exibe o resultado convertido na variavel json
+function carregaElementosNaPagina(json) {
+    // criamos o elemento tabela no html
+    const table = document.createElement("table");
+    // para cada iem 'pessoa' da variavel json
+    // crie um elemento chamado tr (linha) no html
 
-async function carregaPagina(el) {
-    try {
-        const href = el.getAttribute("href");
-        const response = await fetch(href);
+    // criamos uma linha e tres colunas para cada item
+  for (let pessoa of json) {
+    const tr = document.createElement("tr");
+    // nas linhas criadas, crie um elemente chamado td (celula)
+    const td1 = document.createElement("td");
+    // para cada celula, adicione no html a informação do json.nome
+    td1.innerHTML = pessoa.nome;
+    // adicione na tr (linha da tabela) a informação da td (celula da tabela)
+    tr.appendChild(td1);
 
-        if (response.status !== 200) throw new Error('vacilamos');
+    const td2 = document.createElement("td");
+    td2.innerHTML = pessoa.idade;
+    tr.appendChild(td2);
 
-        const html = await response.text();
-        carregaResultado(html);
-    } catch {
-        (error) => console.log(error);
-    }; 
+    const td3 = document.createElement("td");
+    td3.innerHTML = pessoa.salario;
+    tr.appendChild(td3);
+
+    // adicione na tabela criada, todos os intens tr(linha) que ja herdam as td(celula)
+    // aqui montamos a tabela => table - herda tr - herda td
+    table.appendChild(tr);
+  };
+
+  // capturamos a classe resultado (que esta sendo exibido no DOM do html)
+  const resultado = document.querySelector('.resultado');
+  // exibimos a nossa tabela na classe resultado do dom.
+  resultado.appendChild(table);
 };
-
-function carregaResultado(response) {
-    const resultado = document.querySelector(".resultado");
-    resultado.innerHTML = response;
-};
-
